9d39c5ef4fd0116c5043c9d6d254ebe8bf1d7555
'Sungyoun <'sg17990@gamil.com'>
2020-10-12 14:42:01 +0900

    add

--- hairapp/src/main/java/com/yedam/hairshop/common/FileUpload.java
+++ hairapp/src/main/java/com/yedam/hairshop/common/FileUpload.java
@@ -6,13 +6,12 @@ import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.Part;
 
-
 public class FileUpload {
 
 	public static String upload(String path, Part part) {
 		String returnFileName = null;
 		String filename = null;
-		
+
 		if (part == null) {
 			filename = null;
 		} else {
@@ -23,20 +22,22 @@ public class FileUpload {
 				} else {
 					// String path = request.getServletContext().getRealPath("/");
 					String paths = "C:/hairapp" + path;
-					// 파일명 중복체크
-					File renameFile = FileRenamePolicy.rename(new File(paths, filename));
-					try {
-						part.write(path + "/" + renameFile.getName());
-					} catch (IOException e) {
-						e.printStackTrace();
+					if(createFolder(paths)) {
+						// 파일명 중복체크
+						File renameFile = FileRenamePolicy.rename(new File(paths, filename));
+						try {
+							part.write(paths + "/" + renameFile.getName());
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						returnFileName = renameFile.getName();
 					}
-					returnFileName = renameFile.getName();
 				}
 			} catch (UnsupportedEncodingException e) {
 				System.out.println("파일 에러");
 				e.printStackTrace();
 			}
-		}	
+		}
 		return returnFileName;
 	}
 
@@ -48,5 +49,25 @@ public class FileUpload {
 		}
 		return null;
 
+	}
+
+	private static boolean createFolder(String path) {
+		System.out.println(path);
+		File Folder = new File(path);
+		boolean result = false;
+		// 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
+		if (!Folder.exists()) {
+			try {
+				Folder.mkdirs(); // 폴더 생성합니다.
+				System.out.println("폴더가 생성되었습니다.");
+				result = true;
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} else {
+			System.out.println("이미 폴더가 생성되어 있습니다.");
+			result = true;
+		}
+		return result;
 	}
 }
