6c18b1eb108081519519b155a0bfb0cae9070235
kimpoly <kimpoly@localhost>
2020-10-13 13:53:48 +0900

    3

--- hairapp/src/main/java/com/yedam/hairshop/admin/AdminTtCategoryRequestCtrl.java
+++ hairapp/src/main/java/com/yedam/hairshop/admin/AdminTtCategoryRequestCtrl.java
@@ -1,18 +1,25 @@
 package com.yedam.hairshop.admin;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedam.hairshop.common.Controller;
+import com.yedam.hairshop.dao.TtCategoryDAO;
+import com.yedam.hairshop.model.TtCategoryVo;
 
 public class AdminTtCategoryRequestCtrl implements Controller {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		List<TtCategoryVo> list = TtCategoryDAO.getInstance().selectListRequstTmic();
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/admin/ttCategoryRequest.jsp").forward(request, response);
 
 	}
 
